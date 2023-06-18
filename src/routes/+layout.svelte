fb10897038d9a24b67cd80260a10b3f84e3a5ece
Benaiah Barango <benaiah.barango@fullscript.com>
2023-06-18 18:29:08 -0400

    add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
   import "../app.css";
+    import logo from '$lib/assets/tbm-logo.png';
 </script>
 
+<header class="max-w-6xl xl:max-w-screen-2xl mx-auto flex items-center gap-10 justify-between py-4 px-4">
+  <img alt="TBM logo" src={logo} class="h-16 w-[12rem] object-cover" />
+
+  <div class="flex items-center gap-4">
+    <a href="/#about">About</a>
+  </div>
+</header>
 <slot />
+
+
+<footer>Footer</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="relative">
+
+  <div class="grid max-w-6xl xl:max-w-screen-2xl mx-auto items-center gap-10 justify-between py-4 px-4 lg:grid-cols-5">
+    <div class="lg:col-span-3">
+      <h1 class="font-bold text-4xl">The Business Mindset Podcast</h1>
+      <p>On this weekly podcast, we interview black entrepreneurs in Canada about their experiences starting and building their businesses</p>
+    </div>
+
+    <div class="lg:col-span-2">
+      <p>RECENT EPISODES</p>
+      <ul>
+        List
+      </ul>
+    </div>
+  </div>
+</div>
