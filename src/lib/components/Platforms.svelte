7565dd35fd84c4d58ae99071050745cf226bedab
Benaiah Barango <benaiah.barango@fullscript.com>
2023-07-03 10:17:45 -0600

    add youtube badge

--- src/lib/components/Platforms.svelte
+++ src/lib/components/Platforms.svelte
@@ -1,7 +1,7 @@
 <script>
 	import spotify from '$lib/assets/spotify.svg';
 	import google_podcast from '$lib/assets/google_podcast.svg';
-	import apple_podcast from '$lib/assets/apple_podcast.svg';
+	import watch_on_youtube from '$lib/assets/watch_on_youtube.svg';
 	import clsx from 'clsx';
 
 	const platforms = [
@@ -10,15 +10,15 @@
 			title: 'Spotify',
 			href: 'https://open.spotify.com/show/0as6fm4R2X422X03DydlSy'
 		},
-		// {
-		// 	image: apple_podcast,
-		// 	title: 'Apple podcast',
-		// 	href: 'https://podcasts.apple.com/us/podcast/the-business-mindset-podcast/id1693400913'
-		// },
 		{
 			image: google_podcast,
 			title: 'Google podcast',
 			href: 'https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9lMmU0MGM2OC9wb2RjYXN0L3Jzcw'
+		},
+		{
+			image: watch_on_youtube,
+			title: 'Youtube channel',
+			href: 'https://www.youtube.com/@tbm_pod'
 		}
 	];
 
